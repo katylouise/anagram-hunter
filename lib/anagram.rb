608c7c1578ade1e952291efa7c70de40d671f67b
Rebecca Appleyard <rklappleyard@gmail.com>
2015-09-04 13:48:55 +0100

    Our solution

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,9 +2,13 @@
 class AnagramHunter
 
   def initialize(word_list)
+    @word_list = word_list
   end
 
   def call(word)
+    word_perms = word.chars.permutation.to_a.each
+    word_list_arr = word_list.map(&:chars)
+    word_list_arr.select{ |word| word if word_perms.include?(word) }
   end
 
 end
